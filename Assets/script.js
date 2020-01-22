1be4388ec44b309a21c418d237e27750d7ef1f1d
Mauricio Fuentes Gomez <fuentesmauricio75@gmail.com>
2020-01-22 10:35:24 -0600

    copied files from mfg-art.github.io repo

---
+++ Assets/script.js
@@ -0,0 +1,262 @@
+//Variable used to store user input
+var pastSearches = [];
+var cityNames = [];
+var pushToArray = false;
+var pastSearchesObject = {};
+
+//Variables used for display purposes
+var city, date, iconImage, temp, humidity, windSpeed, uvIndex;
+var responseTrue;
+
+//If past search results are stored locally, this will pull up weather information for the most recent search.
+if (localStorage.getItem("pastSearchesJSON")) {
+  pastSearchesObject = JSON.parse(localStorage.getItem("pastSearchesJSON"));
+  pastSearches = pastSearchesObject.pastSearches;
+  userInput = pastSearches[pastSearches.length - 1];
+  getWeather(userInput);
+}
+
+updateButtons();
+
+function updateButtons() {
+  $(".grayColor").empty();
+  if (localStorage.getItem("pastSearchesJSON")) {
+    pastSearchesObject = localStorage.getItem("pastSearchesJSON");
+    pastSearchesObject = JSON.parse(pastSearchesObject);
+    pastSearches = pastSearchesObject.pastSearches;
+    cityNames = pastSearchesObject.cityNames;
+
+    for (var i = 0; i < pastSearches.length; i++) {
+      var newBtn = $(
+        "<button class='btn btn-light newButton m-sm-2'></button>"
+      );
+      newBtn.text(cityNames[i]);
+      $(".grayColor").append(newBtn);
+    }
+  } else {
+    return;
+  }
+}
+
+function getWeather(userInput) {
+  var APIKey = "1b1afd411484586400e59a0b2f0bbe81";
+  var proxy = "https://chriscastle.com/proxy/index.php?:proxy:";
+  var queryURL = "https://api.openweathermap.org/data/2.5/weather?";
+  let lat, long;
+  var datesArray = [];
+  var tempArray = [];
+  var humidityArray = [];
+  var weatherArray = [];
+
+  $.ajax({
+    url: proxy + queryURL,
+    method: "GET",
+    dataType: "json",
+    data: "q=" + userInput + "&appid=" + APIKey,
+    // if ajax call was successful
+    success: function(data) {
+      dataTrue = true;
+      ajaxCall = data;
+      city = data.name;
+      date = data.dt;
+      lat = data.coord.lat;
+      long = data.coord.lon;
+      iconImage = data.weather[0].icon;
+
+      temp = data.main.temp;
+      // converting temp to Fahrenheit
+      temp = (temp - 273.15) * (9 / 5) + 32;
+      temp = temp.toFixed(0);
+
+      humidity = data.main.humidity;
+      windSpeed = data.wind.speed;
+      UVAjaxCall();
+    },
+    error: function(data) {
+      responseTrue = false;
+      console.log("Error!");
+
+      alert("Error! Please enter a valid city name.");
+    }
+  });
+
+  function UVAjaxCall() {
+    console.log(lat + ", " + long);
+    queryURL = "https://api.openweathermap.org/data/2.5/uvi?";
+    $.ajax({
+      url: proxy + queryURL,
+      method: "GET",
+      dataType: "json",
+      data: "appid=" + APIKey + "&lat=" + lat + "&lon=" + long,
+      // if ajax call was successful
+      success: function(data) {
+        responseTrue = true;
+        uvIndex = data.value;
+        ajaxCall = data;
+        console.log(ajaxCall);
+        forecastAjaxCall();
+      },
+      error: function() {
+        responseTrue = false;
+        console.log("Error! UV data not found.");
+      }
+    });
+  }
+
+  function forecastAjaxCall() {
+    queryURL = "https://api.openweathermap.org/data/2.5/forecast?";
+    var index = 0;
+
+    $.ajax({
+      url: proxy + queryURL,
+      method: "GET",
+      dataType: "json",
+      data: "lat=" + lat + "&lon=" + long + "&appid=" + APIKey,
+      success: function(data) {
+        for (var i = 6; i < 39; i += 8) {
+          console.log(data.list[i]);
+          console.log("index: " + i);
+          datesArray[index] = data.list[i].dt;
+          tempArray[index] = data.list[i].main.temp;
+          humidityArray[index] = data.list[i].main.humidity;
+          weatherArray[index] = data.list[i].weather[0].icon;
+          index++;
+        }
+        displayWeatherData();
+      },
+      error: function() {
+        console.log("Error.");
+      }
+    });
+  }
+  //Waits for AJAX call to end.
+  function displayWeatherData() {
+    if (responseTrue) {
+      //Checks if the array is not empty and compares each item in the array with the current user input
+      if (pastSearches.length > 0) {
+        for (var i = 0; i < pastSearches.length; i++) {
+          if (pastSearches[i] !== userInput) {
+            pushToArray = true;
+          } else {
+            pushToArray = false;
+            break;
+          }
+        }
+        //If the array is empty, push the new user input
+      } else {
+        pushToArray = true;
+      }
+
+      if (pushToArray) {
+        pastSearches.push(userInput);
+        cityNames.push(city);
+        pastSearchesObject = {
+          pastSearches: pastSearches,
+          cityNames: cityNames
+        };
+        let pastSearchesJSON = JSON.stringify(pastSearchesObject);
+        localStorage.setItem("pastSearchesJSON", pastSearchesJSON);
+        updateButtons();
+      }
+
+      var weatherDiv = $("<div>");
+      var citySpan = $("<span>");
+      var dateSpan = $("<span>");
+      var iconImgEL = $("<img>");
+      var tempSpan = $("<span>");
+      var humiditySpan = $("<span>");
+      var windSpeedSpan = $("<span>");
+      var UVIndexSpan = $("<span>");
+
+      for (var i = 0; i < 5; i++) {
+        var forecastDiv = $("<div>");
+        var fcDateSpan = $("<span>");
+        var fcTempSpan = $("<span>");
+        var fcHumiditySpan = $("<span>");
+        var fcWeatherIcon = $("<img>");
+        var dateMoment = moment(datesArray[i], "X");
+
+        forecastDiv.attr("class", "forecastDiv");
+        fcDateSpan.attr("class", "fcDateSpan");
+        fcTempSpan.attr("class", "fcTempSpan");
+        fcHumiditySpan.attr("class", "fcHumiditySpan");
+        fcWeatherIcon.attr("class", "fcWeatherIcon");
+        fcDateSpan.html("Date: " + dateMoment.format("MM-DD-YYYY"));
+        fcTempSpan.html("Temperature: " + tempArray[i]);
+        fcHumiditySpan.html("Humidity: " + humidityArray[i] + "%");
+        fcWeatherIcon.attr(
+          "src",
+          "https://openweathermap.org/img/wn/" + weatherArray[i] + ".png"
+        );
+        forecastDiv.append(fcDateSpan);
+        forecastDiv.append(fcTempSpan);
+        forecastDiv.append(fcHumiditySpan);
+        forecastDiv.append(fcWeatherIcon);
+        $(".lightBlueColor").append(forecastDiv);
+      }
+
+      citySpan.text(city);
+      citySpan.attr("class", "citySpan");
+      var dateMoment = moment(date, "X");
+      date = dateMoment.format("LL");
+      dateSpan.text(date);
+      dateSpan.attr("class", "dateSpan");
+      iconImgEL.attr("alt", iconImage);
+      iconImgEL.attr(
+        "src",
+        "https://openweathermap.org/img/wn/" + iconImage + "@2x.png"
+      );
+
+      tempSpan.text(temp + "°F");
+      tempSpan.attr("class", "tempSpan");
+      humiditySpan.text("Humidity: " + humidity + " %");
+      humiditySpan.attr("class", "humiditySpan");
+      windSpeedSpan.text("Wind speed: " + windSpeed);
+      windSpeedSpan.attr("class", "windSpeedSpan");
+      UVIndexSpan.text("UV Index: " + uvIndex);
+      UVIndexSpan.attr("class", "UVIndexSpan");
+
+      weatherDiv.append(citySpan);
+      weatherDiv.append(dateSpan);
+      weatherDiv.append(iconImgEL);
+      weatherDiv.append(tempSpan);
+      weatherDiv.append(humiditySpan);
+      weatherDiv.append(windSpeedSpan);
+      weatherDiv.append(UVIndexSpan);
+      $(".lightBlueColor").append(weatherDiv);
+    }
+  }
+}
+
+$("#submitBtn").on("click", function(event) {
+  event.preventDefault();
+
+  let userInput = $("#search").val();
+  $("#search").val("");
+  // Trims user input and replaces spaces with +.
+  userInput = userInput.trim();
+  userInput = userInput.toLowerCase();
+  while (userInput.includes(" ")) {
+    userInput = userInput.replace(" ", "+");
+  }
+  getWeather(userInput);
+});
+
+$(document).on("click", ".newButton", function(event) {
+  event.preventDefault();
+  let userInput = $(this).text();
+  userInput = userInput.trim();
+  userInput = userInput.toLowerCase();
+  while (userInput.includes(" ")) {
+    userInput = userInput.replace(" ", "+");
+  }
+  getWeather(userInput);
+});
+
+$("#clearSearchHistory").on("click", function(event) {
+  event.preventDefault();
+  localStorage.removeItem("pastSearchesJSON");
+  updateButtons();
+});
+
+updateButtons();
--- README.md
+++ README.md
@@ -1,2 +1,61 @@
-# Weather-Dashboard
-A weather dashboard utilizing OpenWeatherMap API
+# [Weather Dashboard](https://mfg-art.github.io/Homework/06-Weather/)
+
+Project link: [Weather Dashboard](https://mfg-art.github.io/Homework/06-Weather/)
+
+## Table of Contents
+
+- [Description](#description)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Description
+
+This application displays weather information based on user input.
+The first action that will take place after the page has loaded is a search in your local storage for previous user inputs. If a JSON string with key _"pastSearchesJSON"_ is found, its content (an array containing user inputs as strings) will be parsed out and used to generate search buttons on the left side of the page. Clicking each button is equivalent to typing the name of that city in the search bar and hitting enter.
+
+![London button](Assets/first-search-results.png)
+
+**_Clicking the "London" button is equivalent to typing "London" or "london" in the search bar and pressing enter_**
+
+If a city already has a search button associated with it, another one will not be created. User inputs are formatted before use to prevent posssible repeated city buttons. For example, user inputs are all made lowercase, so that "London" and "london" don't create two separate buttons. The array containing past user inputs is scanned through with a for loop to ensure that incoming inputs aren't pushed to it if that same string already exists in it.
+
+The user is prompted to enter a city name and submit their input by either pressing enter or clicking on the submit button. If their input does not contain a valid city name, the user will be prompted to enter a valid city name by an alert.
+
+If a user input is valid, it will be trimmed and formatted so that it can be used as a parameter in an ajax call to _https://openweathermap.org_. This input is stored in a local variable and sent to a function which calls the OpenWeatherMap API. If the search has not been made before, a button will be created in the left side of the page that will retrigger the search.
+
+The search results contain a five day forecast containing date, temperature, humidity, and weather information. Below this, current weather details are displayed. These details include the temperature in Fahrenheit, the humidity, a weather icon representing current weather conditions, wind speed in m/s and the UV index.
+
+![Weather Results Example](Assets/first-search-results.png)
+
+## Usage
+
+This is what the page looks loaded without any past searches stored in local storage:
+
+![Blank Page](Assets/clear-page-no-buttons.png)
+
+Before you submit your first search, the page will look like this:
+
+![Making First Search](Assets/making-first-search.png)
+
+Once you hit enter or click the green submit button, a new button will appear in the left column as well as your search results.
+
+![Weather Results Example](Assets/first-search-results.png)
+
+When other searches are made, they will append below the most recent search results.
+
+![Multiple Weather Search Results](Assets/second-search-results.png)
+
+To clear all search results, click on the gray "Clear Search Results" button.
+
+![Buttons are removed, but the search results are still displayed](Assets/search-results-no-buttons.png)
+
+Clicking on the gray city buttons provides updated weather information.
+
+## Credits
+
+This webpage was created by me, Mauricio Fuentes Gomez. Bootstrap is released under the MIT license and is copyright 2018 Twitter. Copyright 2019 The jQuery Foundation. jQuery License. Free weather API is provided under the terms of the Creative Commons Attribution-ShareAlike 4.0 Generic License.
+
+## License
+
+MIT License
